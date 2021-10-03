a2937e1ec01c14c590501fba7e554ce55c996406
Marcus Hultman <marcus.hultman1@gmail.com>
2021-10-03 14:50:28 +0200

    initial commit

---
+++ apa102.h
@@ -0,0 +1,63 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+#if PI
+#include <wiringPi.h>
+#include <wiringPiSPI.h>
+#endif
+
+namespace apa102 {
+
+char const hex_chars[16] = {
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+
+class LED {
+ public:
+  LED(int num_leds, int hz = 6000000) : _num_leds{num_leds} {
+#if PI
+    wiringPiSetup();
+    if (wiringPiSPISetup(0, hz) < 0) {
+      printf("wiringPiSPISetup failed\n");
+    }
+#endif
+    int trailing = _num_leds / 16;
+    if (_num_leds % 16 != 0) {
+      ++trailing;
+    }
+    int capacity = 4 + 4 * _num_leds + trailing;
+    _buf.resize(capacity);
+    for (auto i = 0; i < _num_leds; ++i) {
+      _buf[(1 + i) * 4] = 0xff;
+    }
+    for (auto i = 0; i < trailing; ++i) {
+      _buf[(1 + _num_leds) * 4 + i] = 0xff;
+    }
+  }
+
+  void set(int i, uint8_t r, uint8_t g, uint8_t b) {
+    printf("%d: %d/%d/%d\n", i, (1 + i) * 4 + 1, (1 + i) * 4 + 2, (1 + i) * 4 + 3);
+    _buf[(1 + i) * 4 + 1] = b;
+    _buf[(1 + i) * 4 + 2] = g;
+    _buf[(1 + i) * 4 + 3] = r;
+  }
+
+  void show() {
+    std::string s;
+    for (auto byte : _buf) {
+      s += "\\x";
+      s += hex_chars[(byte & 0xF0) >> 4];
+      s += hex_chars[(byte & 0x0F) >> 0];
+    }
+    printf("%s\n", s.c_str());
+#if PI
+    wiringPiSPIDataRW(0, _buf, _buf.size());
+#endif
+  }
+
+  int _num_leds;
+  std::vector<uint8_t> _buf;
+};
+
+}  // namespace apa102
---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -eu
+
+ssh pi@192.168.1.66
+
+cd ~/spoticode-apa102
+git pull
+
+mkdir -p ~/spoticode-apa102/build
+cd ~/spoticode-apa102/build
+
+cmake ..
+make
+
+./spoticode
