9236cabd9f4f79964b70f85891ab1a97408ba3c4
Marcus Hultman <marcus.hultman1@gmail.com>
2021-10-03 20:55:36 +0200

    A

--- alpha_map.h
+++ alpha_map.h
@@ -1,6 +1,7 @@
 #include <map>
 #include <vector>
 
-const auto kAlphaMap =  std::map<char, std::vector<std::vector<int>>>{
-  {'A', {{-2, -1, 0, 1}, {0, 2}, {0, 2}, {-2, -1, 0, 1}}},
+const auto kAlphaMap = std::map<char, std::vector<std::vector<int>>>{
+    {'A',
+     {{-4, -3, -2, -1, 0, 1, 2, 3}, {-2, -1, 2, 3}, {-2, -1, 2, 3}, {-4, -3, -2, -1, 0, 1, 2, 3}}},
 };
