8e4e42307e05205e56ba44b14b094c0b6b710ead
Marcus Hultman <marcus.hultman1@gmail.com>
2023-09-02 21:59:20 -0400

    lower brightness duringn the night

--- src/spotiled.cpp
+++ src/spotiled.cpp
@@ -10,6 +10,12 @@
 Coord operator+(const Coord &lhs, const Coord &rhs) { return {lhs.x + rhs.x, lhs.y + rhs.y}; }
 Coord operator*(const Coord &lhs, const Coord &rhs) { return {lhs.x * rhs.x, lhs.y * rhs.y}; }
 
+Color operator*(const Color &lhs, const Color &rhs) {
+  return {static_cast<uint8_t>(static_cast<int>(lhs[0]) * rhs[0] / 255),
+          static_cast<uint8_t>(static_cast<int>(lhs[1]) * rhs[1] / 255),
+          static_cast<uint8_t>(static_cast<int>(lhs[2]) * rhs[2] / 255)};
+}
+
 Color operator*(const Color &c, uint8_t s) {
   return {static_cast<uint8_t>(static_cast<int>(c[0]) * s / 255),
           static_cast<uint8_t>(static_cast<int>(c[1]) * s / 255),
@@ -45,13 +51,22 @@ std::unique_ptr<SpotiLED> SpotiLED::create() {
 
 std::unique_ptr<StaticPresenter> StaticPresenter::create(SpotiLED &led,
                                                          Page &page,
-                                                         uint8_t brightness) {
+                                                         ColorProvider brightness,
+                                                         ColorProvider logo_brightness) {
   class StaticPresenterImpl final : public StaticPresenter {
    public:
-    StaticPresenterImpl(SpotiLED &led, Page &page, uint8_t brightness)
-        : _led{led}, _page{page}, _brightness{brightness} {}
+    StaticPresenterImpl(SpotiLED &led,
+                        Page &page,
+                        ColorProvider brightness,
+                        ColorProvider logo_brightness)
+        : _led{led},
+          _page{page},
+          _brightness{std::move(brightness)},
+          _logo_brightness{std::move(logo_brightness)} {}
 
     void present() {
+      auto brightness = _brightness();
+
       _led.clear();
       for (auto &placement : _page.sprites()) {
         if (!placement.sprite) {
@@ -59,8 +74,8 @@ std::unique_ptr<StaticPresenter> StaticPresenter::create(SpotiLED &led,
         }
         for (auto &section : placement.sprite->sections) {
           for (auto pos : section.coords) {
-            auto [r, g, b] = section.color * _brightness;
-            _led.set(placement.pos + placement.scale * pos, section.color * _brightness);
+            auto [r, g, b] = section.color * brightness;
+            _led.set(placement.pos + placement.scale * pos, section.color * brightness);
           }
         }
       }
@@ -70,9 +85,11 @@ std::unique_ptr<StaticPresenter> StaticPresenter::create(SpotiLED &led,
    private:
     SpotiLED &_led;
     Page &_page;
-    uint8_t _brightness = 0;
+    ColorProvider _brightness;
+    ColorProvider _logo_brightness;
   };
-  return std::make_unique<StaticPresenterImpl>(led, page, brightness);
+  return std::make_unique<StaticPresenterImpl>(led, page, std::move(brightness),
+                                               std::move(logo_brightness));
 };
 
 // rolling_presenter.cpp
@@ -89,28 +106,29 @@ std::unique_ptr<RollingPresenter> RollingPresenter::create(async::Scheduler &sch
                                                            SpotiLED &led,
                                                            Page &page,
                                                            Direction direction,
-                                                           uint8_t brightness,
-                                                           uint8_t logo_brightness) {
+                                                           ColorProvider brightness,
+                                                           ColorProvider logo_brightness) {
   class RollingPresenterImpl final : public RollingPresenter {
    public:
     RollingPresenterImpl(async::Scheduler &scheduler,
                          SpotiLED &led,
                          Page &page,
                          Direction direction,
-                         uint8_t brightness,
-                         uint8_t logo_brightness)
+                         ColorProvider brightness,
+                         ColorProvider logo_brightness)
         : _led{led},
           _page{page},
           _direction{direction},
-          _brightness{brightness},
-          _logo_brightness{logo_brightness},
+          _brightness{std::move(brightness)},
+          _logo_brightness{std::move(logo_brightness)},
           _started{std::chrono::system_clock::now()},
           _render{scheduler.schedule([this] { present(); }, {.period = 200ms})} {}
 
     void present() {
       _led.clear();
-      _led.setLogo({_logo_brightness, _logo_brightness, _logo_brightness});
+      _led.setLogo(_logo_brightness());
 
+      auto brightness = _brightness();
       auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(
           std::chrono::system_clock::now() - _started);
 
@@ -124,7 +142,7 @@ std::unique_ptr<RollingPresenter> RollingPresenter::create(async::Scheduler &sch
         for (auto &section : placement.sprite->sections) {
           for (auto pos : section.coords) {
             auto [r, g, b] = section.color;
-            _led.set(offset + placement.pos + placement.scale * pos, section.color * _brightness);
+            _led.set(offset + placement.pos + placement.scale * pos, section.color * brightness);
           }
         }
       }
@@ -135,11 +153,11 @@ std::unique_ptr<RollingPresenter> RollingPresenter::create(async::Scheduler &sch
     SpotiLED &_led;
     Page &_page;
     Direction _direction;
-    uint8_t _brightness;
-    uint8_t _logo_brightness;
+    ColorProvider _brightness;
+    ColorProvider _logo_brightness;
     std::chrono::system_clock::time_point _started;
     async::Lifetime _render;
   };
-  return std::make_unique<RollingPresenterImpl>(scheduler, led, page, direction, brightness,
-                                                logo_brightness);
+  return std::make_unique<RollingPresenterImpl>(scheduler, led, page, direction,
+                                                std::move(brightness), std::move(logo_brightness));
 }
