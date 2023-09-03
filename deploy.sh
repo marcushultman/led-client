16397bc8080286c60b1c74375bad9d9468ab3264
Marcus Hultman <marcus.hultman1@gmail.com>
2023-09-03 08:40:26 -0400

    deploy.sh: kill & clear process

--- deploy.sh
+++ deploy.sh
@@ -6,10 +6,15 @@ cd ~/spoticode-apa102
 git fetch origin master
 git checkout origin/master
 
+pkill -SIGTERM spoticode || true
+python3 scripts/clear.py
+
 mkdir -p ~/spoticode-apa102/build
 cd ~/spoticode-apa102/build
 
 cmake -DPI=1 ..
 make spoticode
 
-./spoticode
+nohup ./spoticode --brightness=32 >/dev/null 2>&1 &
+
+echo "Done!"
