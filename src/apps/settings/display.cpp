96443ec2f9b4981038f6c4b6ca60f654a932788f
Marcus Hultman <marcus.hultman1@gmail.com>
2023-10-10 22:49:45 -0400

    settings/display: adjust colors

--- src/apps/settings/display.cpp
+++ src/apps/settings/display.cpp
@@ -98,25 +98,25 @@ void DisplayService::start(SpotiLED &led, present::Callback callback) {
     }
     led.setLogo(logoBrightness());
 
-    uint8_t c1 = 3 * _brightness / 4;
-    uint8_t c2 = 4 * _brightness / 5;
+    uint8_t c1 = 1 * _brightness / 4;
+    uint8_t c2 = 1 * _brightness / 3;
 
     for (auto x = 0; x < 23; ++x) {
       for (auto [y, end] = waveIndices(10 * elapsed.count(), _brightness, x, 20, 8, 3); y < end;
            y++) {
-        led.blend({x, y}, 32 * Color{c1, c1, _brightness});
+        led.blend({x, y}, 16 * Color{c1, c1, _brightness});
       }
     }
     for (auto x = 0; x < 23; ++x) {
       for (auto [y, end] = waveIndices(15 * elapsed.count(), _brightness, x, 25, 6, 3); y < end;
            y++) {
-        led.blend({x, y}, 32 * Color{c2, c2, _brightness});
+        led.blend({x, y}, 16 * Color{c2, c2, _brightness});
       }
     }
     for (auto x = 0; x < 23; ++x) {
       for (auto [y, end] = waveIndices(20 * elapsed.count(), _brightness, x, 30, 4, 2); y < end;
            y++) {
-        led.blend({x, y}, 32 * Color{_brightness});
+        led.blend({x, y}, 16 * Color{_brightness});
       }
     }
 
