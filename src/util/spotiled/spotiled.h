b277894e3121f63f242617d2fc3ee26299a38121
Marcus Hultman <marcus.hultman1@gmail.com>
2023-10-11 16:49:22 -0400

    spotiled: remove unused clear & show APIs

--- src/util/spotiled/spotiled.h
+++ src/util/spotiled/spotiled.h
@@ -10,11 +10,10 @@
 
 struct SpotiLED {
   virtual ~SpotiLED() = default;
-  virtual void clear() = 0;
+
   virtual void setLogo(Color) = 0;
   virtual void set(Coord pos, Color) = 0;
   virtual void blend(Coord pos, Color, float blend = 0.5F) = 0;
-  virtual void show() = 0;
 
   using RenderCallback =
       std::function<std::chrono::milliseconds(SpotiLED &, std::chrono::milliseconds elapsed)>;
