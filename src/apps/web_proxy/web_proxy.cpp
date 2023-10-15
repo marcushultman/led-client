f446a28c6df0f5675ccf6d9a27c7dc2b16abcb8c
Marcus Hultman <marcus.hultman1@gmail.com>
2023-10-14 21:24:08 -0400

    web_proxy: pass optional base_url via cmd options for local dev

--- src/apps/web_proxy/web_proxy.cpp
+++ src/apps/web_proxy/web_proxy.cpp
@@ -5,18 +5,20 @@
 namespace web_proxy {
 namespace {
 
-const auto kBaseUrl = "https://spotiled.deno.dev";
+const auto kDefaultBaseUrl = "https://spotiled.deno.dev";
 const auto kHostHeader = "host";
 
 }  // namespace
 
-WebProxy::WebProxy(async::Scheduler &main_scheduler, http::Http &http)
-    : _main_scheduler{main_scheduler}, _http{http} {}
+WebProxy::WebProxy(async::Scheduler &main_scheduler, http::Http &http, std::string base_url)
+    : _main_scheduler{main_scheduler},
+      _http{http},
+      _base_url{base_url.empty() ? kDefaultBaseUrl : std::move(base_url)} {}
 
 http::Lifetime WebProxy::handleRequest(http::Request req,
                                        http::RequestOptions::OnResponse callback) {
   auto suffix = std::string(url::Url(req.url).host.end(), std::string_view(req.url).end());
-  req.url = kBaseUrl + suffix;
+  req.url = _base_url + suffix;
   if (auto it = req.headers.find(kHostHeader); it != req.headers.end()) {
     it->second = url::Url(req.url).host;
   }
