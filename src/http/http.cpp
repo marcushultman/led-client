a6c96478820b0d6914bc5d865fc9c4815de829b4
Marcus Hultman <marcus.hultman1@gmail.com>
2023-10-21 21:34:37 -0400

    http: rewrite using single-threaded multi API

--- src/http/http.cpp
+++ src/http/http.cpp
@@ -2,18 +2,14 @@
 
 #include <curl/curl.h>
 
-#include <array>
 #include <atomic>
 #include <iostream>
-#include <set>
+#include <unordered_map>
 
 #include "http/util.h"
 
 namespace http {
 namespace {
-
-constexpr auto kThreadPoolSize = 4;
-constexpr auto kDefaultTimeout = std::chrono::seconds(5);
 
 void setMethod(CURL *curl, Method method) {
   switch (method) {
@@ -47,56 +43,147 @@ Response::Response(int status) : status{status} {}
 
 Response::Response(std::string body) : status{200}, body{std::move(body)} {}
 
-struct RequestExecutor : std::enable_shared_from_this<RequestExecutor> {
-  RequestExecutor(Request req, RequestOptions opts)
-      : _mcurl{curl_multi_init()}, _req{std::move(req)}, _opts{std::move(opts)} {}
-  ~RequestExecutor() { curl_multi_cleanup(_mcurl); }
+struct RequestState {
+  RequestState(Request &&request, RequestOptions &&opts)
+      : request{std::move(request)}, opts{std::move(opts)} {}
 
-  void performRequest(CURL *curl) {
-    curl_easy_reset(curl);
-    curl_easy_setopt(curl, CURLOPT_URL, _req.url.c_str());
+  Request request;
+  RequestOptions opts;
+  Response response;
+  std::atomic_bool aborted = false;
 
-    setMethod(curl, _req.method);
+  void retain(async::Lifetime work) { _work = std::move(work); }
+
+ private:
+  async::Lifetime _work;
+};
+
+class RequestHandle final {
+ public:
+  RequestHandle(std::shared_ptr<CURLM> curlm, std::shared_ptr<RequestState> request)
+      : _curlm{std::move(curlm)}, _request{std::move(request)} {}
+  ~RequestHandle() {
+    _request->aborted = true;
+    if (auto curl = _curlm.lock()) curl_multi_wakeup(curl.get());
+  }
+
+ private:
+  std::weak_ptr<CURLM> _curlm;
+  std::shared_ptr<RequestState> _request;
+};
+
+class HttpImpl final : public Http {
+ public:
+  explicit HttpImpl(CURLM *curlm)
+      : _curlm{curlm, [](auto curlm) { curl_multi_cleanup(curlm); }},
+        _thread{async::Thread::create("http")} {}
+  ~HttpImpl() { _thread.reset(); }
+
+  Lifetime request(Request request, RequestOptions opts) final {
+    auto state = std::make_shared<RequestState>(std::move(request), std::move(opts));
+
+    state->retain(_thread->scheduler().schedule(
+        [this, state]() mutable { processNewRequest(std::move(state)); }));
+
+    curl_multi_wakeup(_curlm.get());
+
+    return std::make_shared<RequestHandle>(_curlm, state);
+  }
+
+ private:
+  void processNewRequest(std::shared_ptr<RequestState> state) {
+    if (state->aborted) {
+      return;
+    }
+    setupRequest(std::move(state));
+    process();
+  }
+
+  void process() {
+    for (auto it = _requests.begin(); it != _requests.end();) {
+      auto &[curl, state] = *it;
+      if (state->aborted) {
+        curl_multi_remove_handle(_curlm.get(), curl);
+        curl_easy_cleanup(curl);
+        it = _requests.erase(it);
+      } else {
+        ++it;
+      }
+    }
+
+    int still_running = 0;
+    auto err = curl_multi_perform(_curlm.get(), &still_running);
+
+    int msgq = 0;
+    while (auto info = curl_multi_info_read(_curlm.get(), &msgq)) {
+      if (info->msg == CURLMSG_DONE) {
+        finishRequest(info->easy_handle, std::move(_requests[info->easy_handle]));
+        _requests.erase(info->easy_handle);
+      }
+    }
+
+    if (!err && still_running) {
+      err = curl_multi_poll(_curlm.get(), nullptr, 0, 1000, NULL);
+    }
+
+    if (err) {
+      std::cerr << "curl failed: " << curl_multi_strerror(err) << std::endl;
+      return;
+    }
+
+    if (still_running) {
+      _work = _thread->scheduler().schedule([this] { process(); });
+    }
+  }
+
+  void setupRequest(std::shared_ptr<RequestState> state) {
+    auto curl = curl_easy_init();
+
+    if (!curl) {
+      return state->retain(state->opts.post_to.schedule([state] {
+        if (!state->aborted) {
+          state->opts.callback(500);
+        }
+      }));
+    }
+    curl_easy_setopt(curl, CURLOPT_URL, state->request.url.c_str());
+
+    setMethod(curl, state->request.method);
 
     curl_slist *headers = nullptr;
-    for (auto &[key, val] : _req.headers) {
+    for (auto &[key, val] : state->request.headers) {
       auto new_headers = curl_slist_append(headers, (key + ": " + val).c_str());
       headers = new_headers ? new_headers : (curl_slist_free_all(headers), nullptr);
     }
     curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
 
-    if (!_req.body.empty()) {
-      curl_easy_setopt(curl, CURLOPT_POSTFIELDS, _req.body.c_str());
+    if (!state->request.body.empty()) {
+      curl_easy_setopt(curl, CURLOPT_POSTFIELDS, state->request.body.c_str());
     }
 
-    curl_easy_setopt(curl, CURLOPT_HEADERDATA, &_response);
+    curl_easy_setopt(curl, CURLOPT_HEADERDATA, &state->response);
     curl_easy_setopt(curl, CURLOPT_HEADERFUNCTION, onHeader);
 
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &_response);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &state->response);
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, onBytes);
 
-    curl_multi_add_handle(_mcurl, curl);
+    curl_multi_add_handle(_curlm.get(), curl);
 
-    auto err = CURLM_OK;
-    auto still_running = 0;
-    do {
-      if (err = curl_multi_perform(_mcurl, &still_running); !err && still_running) {
-        err = curl_multi_poll(_mcurl, nullptr, 0, 1000, nullptr);
+    _requests[curl] = state;
+  }
+
+  void finishRequest(CURL *curl, std::shared_ptr<RequestState> state) {
+    curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &state->response.status);
+    curl_multi_remove_handle(_curlm.get(), curl);
+    curl_easy_cleanup(curl);
+
+    state->retain(state->opts.post_to.schedule([state] {
+      if (!state->aborted) {
+        state->opts.callback(std::move(state->response));
       }
-    } while (!err && still_running && !_aborted);
-
-    curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &_response.status);
-    scheduleResponse(err);
-
-    curl_multi_remove_handle(_mcurl, curl);
+    }));
   }
 
-  void abort() {
-    _aborted = true;
-    curl_multi_wakeup(_mcurl);
-  }
-
- private:
   static size_t onHeader(char *ptr, size_t size, size_t nmemb, void *response) {
     size *= nmemb;
     auto buffer = std::string_view(ptr, size);
@@ -116,74 +203,15 @@ struct RequestExecutor : std::enable_shared_from_this<RequestExecutor> {
     return size;
   }
 
-  void scheduleResponse(CURLMcode err) {
-    _work = _opts.post_to.schedule([this, self = shared_from_this(), err] {
-      if (!_aborted) {
-        if (err) {
-          _opts.callback(500);
-        } else {
-          _opts.callback(std::move(_response));
-        }
-      }
-    });
-  }
-
-  CURLM *_mcurl = nullptr;
-  Request _req;
-  RequestOptions _opts;
-  Response _response;
-  std::atomic_bool _aborted = false;
+  std::shared_ptr<CURLM> _curlm;
+  std::unique_ptr<async::Thread> _thread;
+  std::unordered_map<CURL *, std::shared_ptr<RequestState>> _requests;
   async::Lifetime _work;
-};
-
-class RequestHandle final {
- public:
-  RequestHandle(CURL *curl, async::Scheduler &scheduler, std::shared_ptr<RequestExecutor> exec)
-      : _exec{exec}, _work{scheduler.schedule([curl, exec] { exec->performRequest(curl); })} {}
-  ~RequestHandle() { _exec->abort(); }
-
- private:
-  std::shared_ptr<RequestExecutor> _exec;
-  async::Lifetime _work;
-};
-
-class HttpImpl final : public Http {
- public:
-  explicit HttpImpl(bool &ok) {
-    for (auto &resources : _pool) {
-      auto *curl = curl_easy_init();
-      resources.curl = curl;
-      resources.thread = async::Thread::create("http");
-      ok &= bool(curl);
-    }
-  }
-  ~HttpImpl() {
-    for (auto &resources : _pool) {
-      curl_easy_cleanup(resources.curl);
-      resources.thread.reset();
-    }
-  }
-
-  Lifetime request(Request request, RequestOptions opts) final {
-    auto &[curl, thread] = _pool[_next_thread.fetch_add(1) % kThreadPoolSize];
-    return std::make_unique<RequestHandle>(
-        curl, thread->scheduler(),
-        std::make_shared<RequestExecutor>(std::move(request), std::move(opts)));
-  }
-
- private:
-  struct ThreadResources {
-    CURL *curl = nullptr;
-    std::unique_ptr<async::Thread> thread;
-  };
-  std::array<ThreadResources, kThreadPoolSize> _pool;
-  std::atomic_size_t _next_thread = 0;
 };
 
 std::unique_ptr<Http> Http::create() {
-  bool ok = true;
-  auto http = std::make_unique<HttpImpl>(ok);
-  return ok ? std::move(http) : nullptr;
+  auto curlm = curl_multi_init();
+  return curlm ? std::make_unique<HttpImpl>(curlm) : nullptr;
 }
 
 }  // namespace http
