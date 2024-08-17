7b1771b7de4c22c1acad30e8897ec2a8f863117c
Marcus Hultman <marcus.hultman1@gmail.com>
2024-08-17 14:08:02 -0400

    deploy.sh: don't clear display and pass along deployment args

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ git fetch origin main
 git checkout origin/main
 
 pkill -SIGTERM spoticode || true
-python3 scripts/clear.py
+# python3 scripts/clear.py
 
 mkdir -p ~/spoticode-apa102/build
 cd ~/spoticode-apa102/build
@@ -15,6 +15,6 @@ cd ~/spoticode-apa102/build
 cmake -DPI=1 ..
 make spoticode
 
-nohup bash -c './spoticode > >(/usr/bin/logger -t spoticode) 2> >(/usr/bin/logger -t spotcode-err)' 1>/dev/null 2>/dev/null &
+nohup bash -c "./spoticode $@ > >(/usr/bin/logger -t spoticode) 2> >(/usr/bin/logger -t spotcode-err)" 1>/dev/null 2>/dev/null &
 
 echo "Done!"
